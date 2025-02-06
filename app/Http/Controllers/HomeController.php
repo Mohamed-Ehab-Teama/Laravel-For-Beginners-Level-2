8ed4956db84e71032b8fd324e273983b342b4824
Mohamed Ehab Mohamed Teama <mohamed111.me111@gmail.com>
2025-02-06 20:19:37 +0200

    Interacting with Requests

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,61 @@ class HomeController extends Controller
         // dd( Route::currentRouteName() );
         // dd( Route::currentRouteAction() );
 
+
+
+        // Request Path & Methods
+        // dd($request->path());
+        // dd($request->is('/'));
+        // dd($request->is('/hello'));
+        // dd($request->routeIs('test'));
+        // dd( $request->url() );
+        // dd( $request->fullUrl() );
+        // dd( $request->fullUrlWithoutQuery('name') );
+        // dd( $request->fullUrlWithoutQuery(['name', 'age']) );
+        // dd( $request->method() );
+        // dd( $request->isMethod('get') );
+
+
+
+        // Request Inputs
+        // dd( $request->all() );
+        // dd( $request->collect() );
+        // dd( $request->input() );
+        // dd( $request->query() );
+        // dd( $request->boolean('admin') );
+        // dd( $request->date('dob') );
+        // dd( $request->only('name') );
+        // dd( $request->only(['name', 'age', 'dob']) );
+        // dd( $request->except('name') );
+        // dd( $request->except(['name', 'age', 'dob']) );
+
+
+        // Request Inputs Presence
+        // dd( $request->has('name') );
+        // dd( $request->hasAny(['name', 'age', 'cam']) );
+        // $request->whenHas('name0', function() {
+        //     dump('Name Is Existed');
+        // }, function() {
+        //     dump('Name is not existed');
+        // });
+        
+        // dd( $request->filled('name') );
+        // dd( $request->anyFilled(['name', 'age', 'cam']) );
+        // $request->whenFilled('name', function() {
+        //     dump('Name Is Existed');
+        // }, function() {
+        //     dump('Name is not existed');
+        // });
+
+        // dd( $request->missing('name4') );
+        // $request->whenMissing('name', function() {
+        //     dump('Name is not existed');
+        // }, function() {
+        //     dump('Name Is Existed');
+        // });
+
+
+
         return view('welcome');
     }
 }
